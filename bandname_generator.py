2cd9f45db1774937172151389a20ce50881a0579
Gabriel Santos <88512482+santosgabrieldev@users.noreply.github.com>
2021-09-03 15:25:35 -0300

    Add files via upload

---
+++ bandname_generator.py
@@ -0,0 +1,6 @@
+print("Good Evening! Welcome to the Band Name Generator")
+city = input("First, tell us what's the name of the city you grown up? ")
+pet = input("Second, tell us what's your pet's name? ")
+band= city + pet
+print("You band name could be " ,band)
+print("Thanks for using our website!")
